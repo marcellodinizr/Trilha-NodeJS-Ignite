89e28805a298369a6f26b60736e0180bdfc9655b
marcellodinizr <marcellodinizrocha@gmail.com>
2022-03-21 10:33:05 -0300

    adding route, query and body params

--- Fundamentos/src/index.js
+++ Fundamentos/src/index.js
@@ -2,47 +2,38 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json());
+
+/*
+ * Route Params => Identificar um recurso editar/deletar/buscar
+ * Query Params => Paginação / Filtro
+ * Body Params => Os objetos inserção/alteração (JSON)
+*/
+
 app.get("/courses", (request, response) => {
-  return response.json([
-    "Curso 1",
-    "Curso 2",
-    "Curso 3"
-  ]);
-})
+  const query = request.query;
+  console.log(query);
+  return response.json(["Curso 1", "Curso 2", "Curso 3"]);
+});
 
 app.post("/courses", (request, response) => {
-  return response.json([
-    "Curso 1",
-    "Curso 2",
-    "Curso 3",
-    "Curso 4"
-  ]);
-})
+  const body = request.body;
+  console.log(body);
+  return response.json(["Curso 1", "Curso 2", "Curso 3", "Curso 4"]);
+});
 
 app.put("/courses/:id", (request, response) => {
-  return response.json([
-    "Curso 6",
-    "Curso 2",
-    "Curso 3",
-    "Curso 4"
-  ]);
-})
+  const { id } = request.params;
+  console.log(id);
+  return response.json(["Curso 6", "Curso 2", "Curso 3", "Curso 4"]);
+});
 
 app.patch("/courses/:id", (request, response) => {
-  return response.json([
-    "Curso 6",
-    "Curso 7",
-    "Curso 3",
-    "Curso 4"
-  ]);
-})  
+  return response.json(["Curso 6", "Curso 7", "Curso 3", "Curso 4"]);
+});
 
 app.delete("/courses/:id", (request, response) => {
-  return response.json([
-    "Curso 6",
-    "Curso 2",
-    "Curso 4"
-  ]);
-})
+  return response.json(["Curso 6", "Curso 2", "Curso 4"]);
+});
 
 app.listen(3333);
