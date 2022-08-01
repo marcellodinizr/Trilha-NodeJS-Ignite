e769950f8bcdf10c8371913d58b01fc241e1a258
marcellodinizr <marcellodinizrocha@gmail.com>
2022-08-01 17:25:30 -0300

    creating Category and Repository of Category

--- RentaLX/src/server.ts
+++ RentaLX/src/server.ts
@@ -1,16 +1,11 @@
-import express from "express";
+import express from 'express';
+
+import { categoriesRoutes } from './routes/categorioes.routes';
 
 const app = express();
+
 app.use(express.json());
 
-app.get("/", (request, response) => {
-  return response.json({ message: "Hello World" });
-});
+app.use('/categories', categoriesRoutes);
 
-app.post("/courses", (request, response) => {
-  const { name } = request.body;
-
-  return response.json({ name });
-});
-
-app.listen(3333, () => console.log("Server started on port 3333"));
+app.listen(3333, () => console.log('Server started on port 3333'));
